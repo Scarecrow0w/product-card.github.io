28f9a2aa034a8e0257a6f1db75f121e642021d51
Scarecrow0w <daniil6547@ya.ru>
2021-06-03 12:31:51 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        rose: '#F2DDDE'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
