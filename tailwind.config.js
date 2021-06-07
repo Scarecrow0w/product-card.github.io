e78b51d856621613e6bea3212664792052f40db0
Scarecrow0w <daniil6547@ya.ru>
2021-06-07 09:15:07 +0300

    minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './**/*.html',
+    './**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         rose: '#F2DDDE'
-      }
+      },
+      transitionDuration: {
+        '350': '350ms',
+        '400': '400ms',
+        '450': '450ms',
+       }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      translate: ['group-hover'],
+      scale: ['group-hover'],
+    },
   },
   plugins: [],
 }
